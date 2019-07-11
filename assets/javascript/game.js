b5004898fe03a476bff1f439328e675814efbe7e
dwlam8790 <49571438+dwlam8790@users.noreply.github.com>
2019-07-10 21:04:58 -0400

    added more with bootstrap and to the main screen play

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,10 @@
 
-        alert("Save the Princess from the Dragon. Choose your warrier and fight against the dragons!"); 
+    alert("Save the Princess from the Dragon. Choose your warrier and fight against the dragons!"); 
+    var baseAttack = 0;
+    var warrior;
+    var dragon; 
+    var charArray = []; //storing the charaters objects 
+// flag to make sure the player picks a player and opponent 
+    var warriorSelected = false;
+    var dragonSelected = false;
+    
