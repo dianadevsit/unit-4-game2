ecb95166d0cb2c6a0b912cde4ed1b52ed33df501
dwlam8790 <49571438+dwlam8790@users.noreply.github.com>
2019-07-15 20:10:36 -0400

    added more to the jquery and javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,3 +8,29 @@
     var warriorSelected = false;
     var dragonSelected = false;
     
+//function for the game: name, hp, ap. constructing with this. 
+    function Charater(name, hp, ap) {
+        this.name = name;
+        this.healthPoints = hp;
+        this.attackPower = ap;
+    }
+
+//increasung the attack strength to this attack strength + original attack strength
+Charater.prototype.increaseAttack = function () {
+    this.attackPower += baseAttack;
+//+= in JavaScript (as well as in many other languages) adds the right hand side to the variable on the left hand side, storing the result in that variable. 
+};
+
+//attack
+Charater.prototype.attack = function (Obj) {
+    $("#msg").html("You attacked " + Obj.name + "for " + this.attackPower + " damage points.");
+    this.increaseAttack();
+}
+
+//initialize charactors -- should it be "init(Charaters)"? 
+function initCharacters () {
+    var warrior1 = new Charater ("Warrior 1", 200, 75, 25, "./assets/images/warrior1.jpg");
+    var warrior2 = new Charater ("Warrior 2", 100, 10, 5, "./assets/images/warrior2.jpg");
+    var warrior3 = new Charater ("Warrior 3", 150, 30, 12, "./assests/images/warrior3.jpg");
+}
+//init function is called a constructor function, and we can invoke it by using the special Javascript keyword new .
